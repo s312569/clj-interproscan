8f977a25284796e91ef40428ed7370adc8256a83
Jason Mulvenna <jason.mulvenna@gmail.com>
2016-08-16 19:09:40 +1000

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-interproscan "0.1.8"
+(defproject clj-interproscan "0.1.9"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
